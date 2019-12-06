3d39506b5e8acb95966bfc2c269291de7640ab85
Max Marrone <max@marrone.nyc>
2019-12-05 21:06:50 -0500

    Repurpose Startup.s to set up a C runtime environment, which currently does nothing.

--- Source/Startup.s
+++ Source/Startup.s
@@ -13,54 +13,34 @@
 .text
 
 .thumb_func
-main:
-	@ Enable GPIOC clocking on the APB2 bus.
-	.equ RCC_BASE, 0x40021000
-	.equ RCC_APB2ENR_OFFSET, 0x18
-	.equ RCC_APB2ENR_GPIOC_BIT, 4
-	ldr r0, =(RCC_BASE+RCC_APB2ENR_OFFSET)
-	ldr r1, =(1<<RCC_APB2ENR_GPIOC_BIT)
-	str r1, [r0]
+startup:
+	@ Copy .data from ROM to RAM.
+	ldr r0, =image_data_ram_origin
+	ldr r1, =image_data_ram_end
+	ldr r2, =image_data_rom_origin
+data_copy_loop:
+	cmp r0, r1
+	ittt lo
+	ldrblo r3, [r0], 1
+	strblo r3, [r2], 1
+	blo data_copy_loop
 	
-	@ Configure GPIO port C13.
-	.equ GPIOC_BASE, 0x40011000
-	.equ GPIO_CRH_OFFSET, 0x4
-	.equ PC13_CNF_MODE, 0b0001 @ General purpose, push-pull, output, maximum speed 10 MHz.
-	.equ GPIO_CRH_P13_SHIFT, 20 @ Index of the LSb of the [CNF,MODE] bits corresponding to port 13, in GPIOC_CRH.
-	.equ GPIO_ODR_OFFSET, 0xc
-	ldr r0, =(GPIOC_BASE+GPIO_CRH_OFFSET)
-	ldr r1, [r0]
-	mov r2, PC13_CNF_MODE
-	bfi r1, r2, GPIO_CRH_P13_SHIFT, 4
-	str r1, [r0]
+	@ Zero out the part of RAM corresponding to .bss.
+	ldr r0, =image_bss_ram_origin
+	ldr r1, =image_bss_ram_end
+	mov r2, 0
+bss_copy_loop:
+	cmp r0, r1
+	itt lo
+	strblo r2, [r0], 1
+	blo bss_copy_loop
 	
-	@ RM0008 7.2.6 "After a system reset, the HSI oscillator is selected as system clock."
-	@ HSI is 8 MHz.  But, by default, the SysTick clock source is divided by 8 (PM0056 4.5.1).
-	.equ CLOCKS_PER_SECOND, 1000000
-	.equ CLOCKS_PER_STATE_CHANGE, CLOCKS_PER_SECOND / 4 @ 2 blinks per second.
-	.equ STK_LOAD, 0xe000e014
-	.equ STK_CTRL, 0xe000e010
+	@ Jump into C.
+	ldr r0, =main
+	bx r0
 	
-	ldr r0, =STK_LOAD
-	ldr r1, =CLOCKS_PER_STATE_CHANGE
-	str r1, [r0]
-	ldr r0, =STK_CTRL
-	mov r1, 1
-	str r1, [r0]
-	
-change_led_state:
-	ldr r0, =STK_LOAD
-wait_for_systick:
-	ldr r1, [r0]
-	tst r1, (1<<16)
-	mov r1, 1
-	str r1, [r0]
-	beq wait_for_systick
-
-	ldr r0, =(GPIOC_BASE+GPIO_ODR_OFFSET)
-	ldr r1, [r0]
-	eor r1, (1<<13)
-	str r1, [r0]
+	@ In case main() ever returns.
+	b spin
 	
 .thumb_func
 spin:
